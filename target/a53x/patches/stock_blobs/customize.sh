f955df1493d851c932c68424cc875a55efaa2612
Majaahh <maja.blaszczak@proton.me>
2025-11-20 13:15:18 +0100

    target: a53x: stock_blobs: init
    Change-Id: I44dad716e73d098e8b13d91f564b3061026f57d0
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a53x/patches/stock_blobs/customize.sh
@@ -0,0 +1,11 @@
+LOG_STEP_IN "- Replacing Audio blobs"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundAlive_play_plus_ver400.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundAlive_play_plus_ver400.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Replacing Hotword"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM55"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM55"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM4" 0 0 755 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM4" 0 0 755 "u:object_r:system_file:s0"
+LOG_STEP_OUT
