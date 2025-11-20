943730ec53cddcdbea3a8dff14acdf996a42eb67
Majaahh <maja.blaszczak@proton.me>
2025-11-20 01:49:59 +0100

    target: a53x: init
    * installer/customize.sh is prepared for other models support
    
    Change-Id: Ia4db9dbeb3cc5a7863d7abc87811c49249d2e12b
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a53x/config.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 Majaahh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A53 5G (a53x)
+TARGET_NAME="Galaxy A53 5G"
+TARGET_CODENAME="a53x"
+TARGET_PLATFORM="s5e8825"
+TARGET_ASSERT_MODEL=("SM-A536B")
+TARGET_FIRMWARE="SM-A536B/EUX/355490891351086"
+TARGET_PRODUCT_SHIPPING_API_LEVEL="31"
+
+# SEC Product Feature
+TARGET_CAMERA_SUPPORT_CUTOUT_PROTECTION=true
+TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME="siop_a53x_s5e8825"
+TARGET_FINGERPRINT_CONFIG_SENSOR="google_touch_display_optical,settings=3,no_delay_in_screen_off"
---
+++ target/a53x/installer/customize.sh
@@ -0,0 +1,25 @@
+REPOSITORY="https://github.com/majaahh/proprietary_vendor_samsung_exynos/releases/download"
+A536B_TAG="A536BXXSHFYI1_EUX_OXM"
+
+for i in "A536B"; do
+    MODEL_TAG_NAME="${i}_TAG"
+    eval "MODEL_TAG=\${$MODEL_TAG_NAME}"
+    ZIP_ARCHIVE="$(echo "$MODEL_TAG" | cut -d'_' -f1)_BL_CP-los"
+    BOOTLOADER_BLOBS="\"fld.bin_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"harx.bin_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"keystorage.bin_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"ldfw.img_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"modem.bin_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"modem_debug.bin_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"sboot.bin_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"tzar.img_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"tzsw.img_SM-$i\" "
+    BOOTLOADER_BLOBS+="\"uh.bin_SM-$i\""
+
+    LOG "- Downloading $ZIP_ARCHIVE.zip"
+    DOWNLOAD_FILE "$REPOSITORY/$MODEL_TAG/$ZIP_ARCHIVE.zip" "$TMP_DIR/$ZIP_ARCHIVE.zip" || return 1
+
+    LOG "- Extracting $ZIP_ARCHIVE.zip"
+    EVAL "unzip \"$TMP_DIR/$ZIP_ARCHIVE.zip\" -d \"$TMP_DIR\" $BOOTLOADER_BLOBS" || return 1
+    EVAL "rm -f \"$TMP_DIR/$ZIP_ARCHIVE.zip\""
+done
