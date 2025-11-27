cf61dd7c1d3b27768589a484213b97227d776ad4
Flopster101 <nahuelgomez329@gmail.com>
2025-11-27 22:58:09 +0100

    platform: s5e8825: bt-lib-patch: init
    Change-Id: Id6c04793ac7868d417b45e5c3bfb4563dd3395e3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ platform/s5e8825/patches/bt-lib-patch/customize.sh
@@ -0,0 +1,33 @@
+# unica/patches/bt-lib-patch/customize.sh
+if [ ! -f "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" ]; then
+    LOG_STEP_IN "- Extracting libbluetooth_jni.so from com.android.bt.apex"
+
+    if [ -d "$TMP_DIR" ]; then
+        EVAL "rm -rf \"$TMP_DIR\""
+    fi
+    mkdir -p "$TMP_DIR"
+
+    EVAL "unzip -j \"$WORK_DIR/system/system/apex/com.android.bt.apex\" \"apex_payload.img\" -d \"$TMP_DIR\""
+
+    if ! sudo -n -v &> /dev/null; then
+        LOG "\033[0;33m! Asking user for sudo password\033[0m"
+        if ! sudo -v 2> /dev/null; then
+            ABORT "Root permissions are required to unpack APEX image"
+        fi
+    fi
+
+    mkdir -p "$TMP_DIR/tmp_out"
+    EVAL "sudo mount -o ro \"$TMP_DIR/apex_payload.img\" \"$TMP_DIR/tmp_out\""
+    EVAL "sudo cat \"$TMP_DIR/tmp_out/lib64/libbluetooth_jni.so\" > \"$WORK_DIR/system/system/lib64/libbluetooth_jni.so\""
+
+    EVAL "sudo umount \"$TMP_DIR/tmp_out\""
+    rm -rf "$TMP_DIR"
+
+    SET_METADATA "system" "system/lib64/libbluetooth_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+    LOG_STEP_OUT
+fi
+
+LOG_STEP_IN "- Disabling VBR AAC A2DP codec"
+HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" "1f000072a8069f1a1f090071" "1f000072280080521f090071"
+LOG_STEP_OUT
