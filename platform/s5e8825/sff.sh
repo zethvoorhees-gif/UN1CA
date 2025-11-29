129c6170736d8a9dcea595b6a6328c5c43eb410e
Majaahh <maja.blaszczak@proton.me>
2025-11-29 15:33:14 +0100

    platform: s5e8825: init
    Change-Id: I5bb41806271ab3e46e1660d7853e535c8b0c7d46
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ platform/s5e8825/sff.sh
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2025 Majaahh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# SEC Floating Feature configuration file for Exynos 1280 devices (s5e8825)
+
+# Gallery
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
+
+# Refresh Rate
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=1
