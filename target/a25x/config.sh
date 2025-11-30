45d3b1642f41e4d53da9e070e9ff39b6e7a747a5
Majaahh <maja.blaszczak@proton.me>
2025-11-30 16:54:19 +0100

    target: a25x: init
    Change-Id: Ic7f4165c80fd6e7a3c5354de5462436412e48733
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a25x/config.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 Majaahh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A25 5G (a25x)
+TARGET_NAME="Galaxy A25 5G"
+TARGET_CODENAME="a25x"
+TARGET_PLATFORM="s5e8825"
+TARGET_ASSERT_MODEL=("SM-A256E")
+TARGET_FIRMWARE="SM-A256E/XNZ/358385396288808"
+TARGET_PRODUCT_SHIPPING_API_LEVEL="34"
+
+# SEC Product Feature
+TARGET_CAMERA_SUPPORT_CUTOUT_PROTECTION=false
+TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME="siop_a25x_s5e8825"
+TARGET_FINGERPRINT_CONFIG_SENSOR="google_touch_side,settings=3"
