c60601032c5b9b1a3635842daa1291f2cf17d8b0
Majaahh <maja.blaszczak@proton.me>
2025-12-02 01:34:30 +0100

    target: m34x: init
    Change-Id: I00ea592f5cf081d6ad7c90b14fc9b2bc7188f363
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/m34x/config.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 Majaahh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy M34 5G (m34x)
+TARGET_NAME="Galaxy M34 5G"
+TARGET_CODENAME="m34x"
+TARGET_PLATFORM="s5e8825"
+TARGET_ASSERT_MODEL=("SM-M346B2")
+TARGET_FIRMWARE="SM-M346B2/TUR/358812710029659"
+TARGET_PRODUCT_SHIPPING_API_LEVEL="33"
+
+# SEC Product Feature
+TARGET_CAMERA_SUPPORT_CUTOUT_PROTECTION=false
+TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME="siop_m34x_s5e8825"
+TARGET_FINGERPRINT_CONFIG_SENSOR="google_touch_side,settings=3"
