d717c4b276036c9e31e6b42b540d14f1a485591e
Majaahh <maja.blaszczak@proton.me>
2025-12-21 23:11:22 +0100

    [DNM] target: a53x: blobs: grab a54x bt apex
    Change-Id: Ia5d42725b0fc37941f32d2e2bf616139dfda686d
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- target/a53x/patches/blobs/customize.sh
+++ target/a53x/patches/blobs/customize.sh
@@ -35,6 +35,10 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libaudiosaplus_sec_leg
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 LOG_STEP_OUT
 
+LOG_STEP_IN "- Replacing bluetooth apex"
+ADD_TO_WORK_DIR "a54xnsxx" "system" "system/apex/com.android.bt.apex" 0 0 644 "u:object_r:system_file:s0"
+LOG_STEP_OUT
+
 LOG_STEP_IN "- Replacing Camera blobs"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libHpr_RecFace_dl_v1.0.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
